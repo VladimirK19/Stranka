3141c93eb2b7cf30963a3279831305d2ce33eed3
Vlad1m1r19 <player19centrum.skgmail.com>
2024-03-10 00:07:06 +0100

    HTML->PHP+header+footer

---
+++ Particion/footer.php
@@ -0,0 +1,35 @@
+<footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-8 offset-md-2">
+                     <ul class="location_icon">
+                        <li><a href="contact.php"><i class="fa fa-map-marker" aria-hidden="true"></i></a><br> Contact</li>
+                        <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i></a><br> +421 909214214</li>
+                        <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a><br> Sungla@gmail.com</li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>© 2019 All Rights Reserved. Design by<a href="#"> Vladimir Kupec</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script>
+   </body>
+</html>
